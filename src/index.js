82a3aca75a83373404878ea6b552baa11bae424c
jinac102 <monet158@naver.com>
2023-07-24 00:54:17 +0900

    login 3차 커밋

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const logForm = document.querySelector("#login_form");
 const logInput = logForm.querySelector("input");
 const logBtn = logForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-function ClickBtn() {
+const hidden_CL = "hidden";
+
+function ClickBtn(e) {
+  e.preventDefault();
+  logForm.classList.add(hidden_CL);
   const userName = logInput.value;
-
+  greeting.innerText = `Hello ${userName}`;
+  logForm.classList.remove(hidden_CL);
   //consolie.dir()은 ()사이에 든 태그가 가진 정보를 전부 보여준다.
 }
 
-logBtn.addEventListener("click");
+logBtn.addEventListener("click", ClickBtn);
