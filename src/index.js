0223272e584210729a3af9f4b164df88ccac9865
jinac102 <monet158@naver.com>
2023-07-24 00:03:56 +0900

    loginform01

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const logForm = document.querySelector("#login_form");
+const logInput = logForm.querySelector("input");
+const logBtn = logForm.querySelector("button");
+
+function ClickBtn() {
+  console.log();
+}
+
+logBtn.addEventListener("click");
