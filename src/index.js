9fcb96629ce898af9b3b6f004e1b5feac5072e53
jinac102 <monet158@naver.com>
2023-07-24 00:23:36 +0900

    23.0724 no02

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const logInput = logForm.querySelector("input");
 const logBtn = logForm.querySelector("button");
 
 function ClickBtn() {
-  console.log();
+  const userName = logInput.value;
+
+  //consolie.dir()은 ()사이에 든 태그가 가진 정보를 전부 보여준다.
 }
 
 logBtn.addEventListener("click");
